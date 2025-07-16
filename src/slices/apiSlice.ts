166037819dc544c7dc04c155ceb7753434a1a040
AbelGetu <abelgetu27@gmail.com>
2025-07-16 09:30:09 +0300

    adding prepareHeaders to apiSlice fetchBaseQuery to access protected routes

--- src/slices/apiSlice.ts
+++ src/slices/apiSlice.ts
@@ -1,8 +1,19 @@
+import type { RootState } from '@/store'
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 const baseQuery = fetchBaseQuery({ 
     baseUrl: 'http://localhost:8000',
     // credentials: 'include'
+      prepareHeaders: (headers, { getState }) => {
+        // Get the token from the state
+        const token = (getState() as RootState).auth.token
+     
+        if (token) {
+            headers.set('Authorization', `Bearer ${token.access_token}`)
+        }
+
+        return headers
+    }
 })
 
 export const apiSlice = createApi({
